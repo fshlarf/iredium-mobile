7083921daaba4bd05b80a383d92cf5f3a74201b7
fshlarf <faishal.arif218@gmail.com>
2019-10-05 19:00:13 +0700

    add Login and Register screens

---
+++ src/screens/Register/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { View, Text, Image, StatusBar } from 'react-native'
+
+class RegisterScreen extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <View>
+        <Text>Hello from Login screen</Text>
+      </View>
+    )
+  }
+}
+
+export default RegisterScreen
